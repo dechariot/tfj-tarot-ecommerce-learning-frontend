4207402a3da6feec8218f5217d2d14f140bed8dd
dechariot <nsprisang@gmail.com>
2020-06-03 17:18:20 +0700

    Fix create category bug

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -51,7 +51,7 @@ const AddCategory = () => {
 
   const showSuccess = () => {
     if (success) {
-      return <h3 className="text-success">{name} is successfully created</h3>;
+      return <h3 className="text-success">Category is successfully created</h3>;
     }
   };
 
