6c779663e3dd9c286ef8f070ab5e98a317f3e23b
dechariot <nsprisang@gmail.com>
2020-06-03 18:18:04 +0700

    Add show products on homepage

---
+++ src/core/Card.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ShowImage from "./ShowImage";
+
+const Card = ({ product }) => {
+  return (
+    <div className="col-4 mb-3">
+      <div className="card">
+        <div className="card-header">{product.name}</div>
+        <div className="card-body">
+          <ShowImage item={product} url="product" />
+          <p>${product.price}</p>
+          <div className="row">
+            <div className="col-6">
+              <Link to="/">
+                <button className="btn btn-primary mt-2 mb-2">
+                  View Product
+                </button>
+              </Link>
+            </div>
+            <div className="col-6">
+              <Link to="/">
+                <button className="btn btn-warning mt-2 mb-2">
+                  Add to Cart
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/core/Home.js
+++ src/core/Home.js
@@ -1,11 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "./Layout";
+import { getProducts } from "./apiCore";
+import Card from "./Card";
 
-const Home = () => (
-  <Layout
-    title="Home Page"
-    description="The Fool Journey E-commerce App"
-  >Content coming soon</Layout>
-);
+const Home = () => {
+  const [productsBySell, setProductsBySell] = useState([]);
+  const [productsByArrival, setProductsByArrival] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadProductsBySell = () => {
+    getProducts("sold").then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsBySell(data);
+      }
+    });
+  };
+
+  const loadProductsByArrival = () => {
+    getProducts("createdAt").then((data) => {
+      console.log(data);
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProductsByArrival(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadProductsBySell();
+    loadProductsByArrival();
+  }, []);
+
+  return (
+    <Layout
+      title="Home Page"
+      description="The Fool Journey E-commerce App"
+      className="container"
+    >
+      <h2 className="mb-4">Bestselling</h2>
+      <div className="row">
+        {productsBySell.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+      </div>
+
+      <hr></hr>
+
+      <h2 className="mb-4">LastArrival</h2>
+      <div className="row">
+        {productsByArrival.map((product, i) => (
+          <Card key={i} product={product} />
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
 export default Home;
